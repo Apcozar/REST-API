c5b68809c85f793f468a931c07a5e7265ce68694
Ángel Paderne <a.padernecozar@gmail.com>
2023-01-02 13:14:11 +0100

    test

---
+++ src/models/friendships.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from ..database import Base
+
+
+class Friendships(Base):
+    __tablename__ = "friendships"
+
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), primary_key=True)
+    friend_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), primary_key=True)
+    creationTime = Column(TIMESTAMP(timezone=True), nullable=False, 
+                    server_default=text("now()"))
+                    
+
+
--- src/models/users.py
+++ src/models/users.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from .database import Base
+from ..database import Base
 
 
 class Users(Base):
@@ -15,13 +15,3 @@ class Users(Base):
                     server_default=text("now()"))
     age = Column(Integer, nullable=False)
     gender = Column(String(length=20), nullable=False)
-
-
-class Friendships(Base):
-    __tablename__ = "friendships"
-
-    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), primary_key=True)
-    friend_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), primary_key=True)
-    creationTime = Column(TIMESTAMP(timezone=True), nullable=False, 
-                    server_default=text("now()"))
-                    
